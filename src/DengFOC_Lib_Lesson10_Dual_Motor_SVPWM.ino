125f0c34d9137e7f3a64fdbeaff4b64c25a9f7c0
GaryLeung <819421619@qq.com>
2024-03-20 12:03:41 +0800

    feat: 阻尼旋钮与顺滑旋钮

--- src/DengFOC_Lib_Lesson10_Dual_Motor_SVPWM.ino
+++ src/DengFOC_Lib_Lesson10_Dual_Motor_SVPWM.ino
@@ -7,15 +7,7 @@
 
 #include "DengFOC.h"
 #include<math.h>
-#define RATCHET_NUM   4
-#define _PI 3.14159265359f
-#define ANGLE_RANGE  ( _PI )  
 
-float attractor_distance = 0;
-float m0_limit_left = 0;
-float m0_limit_right = 0;
-float m1_limit_left = 0;
-float m1_limit_right = 0;
 
 void setup()
 {
@@ -26,58 +18,13 @@ void setup()
     DFOC_Vbus(12); // 设定驱动器供电电压
     DFOC_M0_alignSensor(7, -1);
     DFOC_M1_alignSensor(7, -1);
-
-    attractor_distance = 2 * _PI / RATCHET_NUM;
-
-    m0_limit_left = S0_zero_electric_angle - ANGLE_RANGE / 2;
-    m0_limit_right = S0_zero_electric_angle + ANGLE_RANGE / 2;
-    m1_limit_left = S1_zero_electric_angle - ANGLE_RANGE / 2;
-    m1_limit_right = S1_zero_electric_angle + ANGLE_RANGE / 2;
-
 }
 
 int count = 0;
 void loop()
 {
-    float target0 = 0;
-    float target1 = 0;
-    float angle0 =0;
-    float angle1 =0;
-
-
     runFOC();
-    angle0 = DFOC_M0_Angle();
-    angle1 = DFOC_M1_Angle();
 
-    if(angle0 < m0_limit_right && angle0 > m0_limit_left){
-        target0 = round(angle0 / attractor_distance )*attractor_distance;
-    }else{
-        if(angle0 > m0_limit_right){
-            target0 = m0_limit_right;
-        }
-        if(angle0 < m0_limit_left){
-            target0 = m0_limit_left;
-
-        }
-    }
-
-    if(angle1 < m1_limit_right && angle1 > m1_limit_left){
-        target1 = round(angle1 / attractor_distance )*attractor_distance;
-    }else{
-        if(angle1 > m1_limit_right){
-            target1 = m1_limit_right;
-        }
-        if(angle1 < m1_limit_left){
-            target1 = m1_limit_left;
-        }
-    }
-
-    DFOC_M0_SET_ANGLE_PID(0.02,0.001,0.001,100000,1.2);
-    DFOC_M0_SET_CURRENT_PID(0.75,9,0,100000);
-    DFOC_M0_set_Force_Angle(target0);
-    DFOC_M1_SET_ANGLE_PID(0.02,0.001,0.001,100000,1.2);
-    DFOC_M1_SET_CURRENT_PID(0.75,9,0,100000);
-    DFOC_M1_set_Force_Angle(target1);
 
 
     // //力位（加入电流环后）
@@ -108,12 +55,23 @@ void loop()
     // DFOC_M1_SET_CURRENT_PID(0.55, 8, 0, 100000);
     // DFOC_M1_set_Velocity_Angle(serial_motor_target());
 
-    // // 电流力矩
+    // 电流力矩
     // DFOC_M1_SET_CURRENT_PID(5, 200, 0, 100000);
     // DFOC_M0_SET_CURRENT_PID(5, 200, 0, 100000);
-
     // DFOC_M0_setTorque(serial_motor_target());
     // DFOC_M1_setTorque(serial_motor_target());
+
+    //阻尼knob 产生反向力矩 阻碍向前旋转
+    DFOC_M0_SET_VEL_PID(0.5,0,0,1000,0.8);
+    DFOC_M0_SET_CURRENT_PID(0.4,5,0,100000);
+    DFOC_M0_setTorque(DFOC_M0_VEL_PID(-1 * DFOC_M0_Velocity()));
+
+    //顺滑knob 产生正向力矩 推动向前旋转
+    DFOC_M1_SET_VEL_PID(0.001,0,0,1000,0.8);
+    DFOC_M1_SET_CURRENT_PID(0.75,5,0,100000);
+    DFOC_M1_setTorque(DFOC_M1_VEL_PID(DFOC_M1_Velocity()));
+
+
 
     count++;
     if (count % 100 == 0) {
